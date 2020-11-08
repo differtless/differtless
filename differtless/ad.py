1e2be59096a3d55847760e2334c63c73f3b49afd
Mark Penrod <mark.penrod@gmail.com>
2020-11-08 12:21:47 -0500

    Drafted __add__ overwrite

--- differtless/ad.py
+++ differtless/ad.py
@@ -32,4 +32,26 @@ class FuncInput():
 
     def __repr__(self):
         return f'FuncInput({sel.val_}, {sel.ders_})'
+
+    """
+    Overwritten basic functions
+    """
+
+    def __add__(self, other):
+        if isinstance(other, FuncInput):
+            new_val = self.val_ + other.val_
+
+            if len(self.ders_) == len(other.ders_):
+                new_ders = [self.ders_[i] + other.ders_[i] for i in range(len(self.ders_))]
+            else:
+                raise ValueError('Both inputs must have the same amount of partial derivatives')
+        else:
+            try:
+                new_val = self.val_ + other
+                new_ders = self.ders_
+            except TypeError:
+                print('ERROR: Inputs must be type FuncInput, int, or float')
+                return
+
+        return FuncInput(new_val, new_ders)
         
