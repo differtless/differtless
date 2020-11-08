51f2432fc735133cea610aac760674011f40dcef
Mark Penrod <mark.penrod@gmail.com>
2020-11-08 12:43:18 -0500

    Trying out a wrapper to make sure inputs are valid

--- differtless/ad.py
+++ differtless/ad.py
@@ -20,6 +20,10 @@ are overwritten so that they will return a FuncInput object with updated value a
 TODO: Write actual DocString
 
 """
+# ADDITIONAL DEPENDENCY (good catchall for checking if it's a number)
+import numbers
+
+
 
 class FuncInput():
 
@@ -33,10 +37,35 @@ class FuncInput():
     def __repr__(self):
         return f'FuncInput({sel.val_}, {sel.ders_})'
 
+
+    # Wrapper that will make sure certain specifications are met
+    def verify_input(self, other):
+        def wrapper(self, other):
+            if isinstance(other, FuncInput):
+                # make sure same amount of derivatives
+                if len(self.ders_) != len(other.ders_):
+                    raise ValueError('Both inputs must have the same number of inputs')
+                # make sure other's value is a real number
+                elif not isinstance(other.val_, numbers.Real):
+                    raise TypeError('FuncInput value must be a real number')
+
+                # make sure all derivatives are real numbers
+                for der in other.ders_:
+                    if  not isinstance(der, numbers.Real):
+                        raise TypeError('All gradient values must be real numbers')
+            # if not funcinput, make sure other is  real number
+            elif not isinstance(other, numbers.Real):
+                raise TypeError('Inputs must be type FuncInput or a real number')
+
+
+
+
     """
     Overwritten basic functions
     """
 
+    # Addition
+    @verify_input
     def __add__(self, other):
         if isinstance(other, FuncInput):
             new_val = self.val_ + other.val_
@@ -54,4 +83,17 @@ class FuncInput():
                 return
 
         return FuncInput(new_val, new_ders)
-        
+
+    # # Multiplication
+    # def __mul__(self, other):
+    #     if isinstance(other, FuncInput):
+    #         new_val = self.val_ * other.val_
+    #
+    #         if len(self.ders_) == len(other.ders_):
+    #             new_ders = [(self.val_ * other.ders_[i]) + (self.ders_[i] * other.val_)for i in range(len(self.ders_))]
+    #         else:
+    #             raise ValueError('Both inputs must have the same amount of partial derivatives')
+    #     else:
+    #         try:
+    #             new_val = self.val_ * other
+    #             new_ders
