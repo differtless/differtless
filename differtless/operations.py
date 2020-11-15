03772822c3339443b4488d22a87e0a6406d83126
Mark Penrod <mark.penrod@gmail.com>
2020-11-15 13:28:32 -0500

    Updated exp function and added wrapper to validate the inputs to the function

--- differtless/operations.py
+++ differtless/operations.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import numbers
 from ad import FuncInput
 
 '''
@@ -10,14 +11,28 @@ Re-defining numpy and scipy functions to return FuncInput objects of (value, gra
 Numpy
 '''
 
-# Exponents and logarithms
+# Wrapper that will make sure certain specifications are met for the inputs
+def validate_input(func):
+    def wrapper(self):
+        if not isinstance(self, FuncInput) or not isinstance(self, numbers.Real):
+            raise TypeError('Inputs must be type FuncInput or a real number')
+        return func(self, other)
+    return wrapper
 
+
+# Exponents and logarithms
+@validate_input
 def exp(x):
-    return FuncInput(np.exp(x), np.exp(x))
+    if isinstance(x, FuncInput):
+        new_vals = np.exp(x.val_)
+        new_ders = x.ders_ * np.exp(x.val_)
+        return FuncInput(new_vals, new_ders)
+    elif isinput(x, numbers.Real):
+        return np.exp(x)
 
 def expm1(x):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def exp2(x):
     raise NotImplementedError('Function not yet implemented in differtless')
 
@@ -26,16 +41,16 @@ def log(x):
 
 def log10(x):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def log1p(x):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def logaddexp(x1, x2):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def logaddexp2(x1, x2):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 # Trigonometric functions
 
 def sin(x):
@@ -55,33 +70,33 @@ def arccos(x):
 
 def arctan(x):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def hypot(x1, x2):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def arctan2(x1, x2):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 # Hyperbolic functions
 
 def sinh(x):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def cosh(x):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def tanh(x):
     raise NotImplementedError('Function not yet implemented in differtless')
 
 def arcsinh(x):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def arccosh(x):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 def arctanh(x):
     raise NotImplementedError('Function not yet implemented in differtless')
-    
+
 '''
 Scipy
 '''
