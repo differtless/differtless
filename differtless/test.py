11a2dff5ed650e0deeb0df3cd985018e29f5373e
Zizhao Will Wang <willwang@Zizhaos-MacBook-Pro.local>
2020-11-16 01:25:08 -0500

    test add

--- differtless/test.py
+++ differtless/test.py
@@ -4,9 +4,9 @@ import numbers
 import pytest
 
 def test_add():
-    x1=ad.FuncInput(1,1)
-    x2=ad.FuncInput(2,1)
-    x3=ad.FuncInput(3,1)
+    x1=ad.FuncInput(1)
+    x2=ad.FuncInput(2)
+    x3=ad.FuncInput(3)
     f = x1+x2+x3
     assert f.val == 6, "error with add"
     assert (f.ders == np.array([1,1,1])).all(), "error with add"
