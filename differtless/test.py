a3edd37bf1b8c841e81b65e4dc0ee3dfac236414
Zizhao Will Wang <willwang@Zizhaos-MacBook-Pro.local>
2020-11-16 21:58:58 -0500

    test sub function

--- differtless/test.py
+++ differtless/test.py
@@ -4,24 +4,18 @@ import numpy as np
 import pytest
 
 def test_add():
-    x1 = FuncInput(np.array([1]),np.array([1,0]))
-    x2 = FuncInput(np.array([2]),np.array([0,1]))
-    f = x1 + x2
-    assert f.val_ == 3, "Add function is not correct"
-    assert (f.ders_ == np.array([1,1])).all(), "Add function is not correct"
+    x1 = FuncInput(np.array([1]),np.array([1,0,0]))
+    x2 = FuncInput(np.array([2]),np.array([0,1,0]))
+    x3 = FuncInput(np.array([3]),np.array([0,0,1]))
+    f = x1 + x2 + x3
+    assert f.val_ == 6, "Add function is not correct"
+    assert (f.ders_ == np.array([1,1,1])).all(), "Add function is not correct"
 
-# def test_exp():
-#     x=FuncInput(np.array([1]),np.array([1]))
-#     f = op.exp(x)
-#     assert f.val_ == 1, "error with add"
-#     assert (f.ders_ == 1).all(), "error with add"
 
-# x1 = FuncInput(np.array([1]),np.array([1,0]))
-# x2 = FuncInput(np.array([2]),np.array([0,1]))
-# f = x1 + x2
-
-# x = FuncInput(np.array([1]),np.array([1]))
-# print(isinstance(x, FuncInput))
-# f = op.exp(x)
-# print(f)
-
+def test_sub():
+    x1 = FuncInput(np.array([1]),np.array([1,0,0]))
+    x2 = FuncInput(np.array([2]),np.array([0,1,0]))
+    x3 = FuncInput(np.array([3]),np.array([0,0,1]))
+    f = x1 - x2 - x3
+    assert f.val_ == -4, "Sub function is not correct"
+    assert (f.ders_ == np.array([1,-1,-1])).all(), "Sub function is not correct"
