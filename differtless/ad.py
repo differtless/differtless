c2245ede136a391f4ee3c8369e96c289fac1d793
Mark Penrod <mark.penrod@gmail.com>
2020-11-18 11:55:42 -0500

    Added function to return Jacobian

--- differtless/ad.py
+++ differtless/ad.py
@@ -333,3 +333,37 @@ def forward(fun, inputs, seeds = []):
     func_inputs = preprocess(inputs, seeds)
 
     return fun(*func_inputs)
+
+def Jacobian(fun, inputs):
+    """
+    Function that executes forward mode of automatic differentiation. Executes a
+    pre-defined function while keeping track of the gradients of the inputs with
+    respect to all other inputs
+
+    PARAMETERS
+    ==========
+        fun:
+            Pre-defined function to be executed
+        inputs : iterable type (list, np.array(), etc.)
+            Iterable containing the input values to the functions
+    ACTIONS
+    =======
+        - Preprocesses inputs to FuncInput type
+        - Execute forward mode with inputs and default seed (identity)
+    RETURNS
+    =======
+        Return the resulting gradients from forward mode which will be the Jacobian
+    EXAMPLE
+    ========
+    >>> inputs = [1, 2]
+    >>> seeds = [[1, 0], [0, 1]]
+    >>> def simple_func(x, y):
+    ...     return (x + y) ** 2
+    >>> forward(simple_func, inputs, seeds)
+    FuncInput([9], [6. 6.])
+    """
+
+    func_inputs = preprocess(inputs)
+    output = fun(*func_inputs)
+
+    return output.gradients
