be6882e2c2442a817d7b9199af196543ce50bc5c
Mark Penrod <mark.penrod@gmail.com>
2020-12-05 17:31:08 -0500

    fixed comparison operators

--- differtless/ad.py
+++ differtless/ad.py
@@ -265,13 +265,13 @@ class FuncInput():
         else:
             raise ValueError('Cannot compare FuncInput to non-FuncInput')
 
-    def le(self, other):
+    def __le__(self, other):
         if isinstance(other, FuncInput):
             return self.val_ <= other.val_
         else:
             raise ValueError('Cannot compare FuncInput to non-FuncInput')
 
-    def ge(self, other):
+    def __ge__(self, other):
         if isinstance(other, FuncInput):
             return self.val_ >= other.val_
         else:
